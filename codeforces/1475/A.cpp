b0c91b4028c51a845325e1d059d0d1ba60f7af77
kunal2812 <kunalkatiyar50@gmail.com>
2021-01-27 18:58:00 +0530

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105576126

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define mp make_pair
+#define pb push_back
+ 
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+ 
+long long mod = 1000000007;
+/*
+for(int i=0;i<N;i++){
+    cout << arr[i] << endl;
+}
+*/
+void Solution(ll N){
+    if((N&(N-1))==0){
+        cout << "NO" << endl;
+    }
+    else{
+        cout << "YES" << endl;
+    }
+}
+ 
+int main() {
+    int T;
+    int i,j,k;
+    ll N;
+    long long A[100000];
+    cin >> T;
+    while(T!=0){
+        std::cin >> N;
+        Solution(N);
+        T--;
+    }
+    return 0;
+}
+ 
