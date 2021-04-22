07da6e1ba052e3e9e6a7f1e267386e7cbf1a3ac3
kunal2812 <kunalkatiyar50@gmail.com>
2021-04-22 18:36:00 +0530

    Add solution for problem `A - Minutes Before the New Year` Link: https://codeforces.com/contest/1283/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1283/submission/113887027

---
+++ codeforces/1283/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define IN for(int i=0;i<n;i++){cin >> a[i];}
+#define OUT for(int i=0;i<n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    int h,m;
+    cin >> h >> m;
+    cout << ((23-h)*60) + (60-m) << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
