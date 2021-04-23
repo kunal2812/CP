ba3afdc2cca390301ec29cf3247fb662f4d836cb
kunal2812 <kunalkatiyar50@gmail.com>
2021-04-23 23:22:00 +0530

    Add solution for problem `A - Review Site` Link: https://codeforces.com/contest/1511/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1511/submission/114044558

---
+++ codeforces/1511/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=0;i<n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    int n;
+    cin >> n;
+    int a[550];
+    IN;
+    int c=0;
+    for(int i=1;i<=n;i++) if(a[i]==3 || a[i]==1) c++;
+    cout << c << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
