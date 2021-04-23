8355cea415f65c6b4a7637bc293dc66587526ce2
Kunal Katiyar <kunalkatiyar50@gmail.com>
2021-04-24 00:16:00 +0530

    Add solution for problem `A - Is it rated - 2` Link: https://codeforces.com/contest/1505/problem/A Tags: *special problem, implementation, interactive, *900 Ref: https://codeforces.com/contest/1505/submission/114049053

---
+++ codeforces/1505/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define IN for(int i=0;i<n;i++){cin >> a[i];}
+#define OUT for(int i=0;i<n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    string s = "";
+    while(1){
+        getline(cin, s);
+        if(s.empty()==true){
+            break;
+        }
+        cout << "NO" << endl << flush ;
+    }
+}
+
+int main() {
+    fast;
+        Solution();
+}
+
