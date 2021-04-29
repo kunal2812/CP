5926cf6d858af635023f868f44d3f29f42b28c71
Kunal Katiyar <kunalkatiyar50@gmail.com>
2021-04-29 20:30:00 +0530

    Add solution for problem `A - Red and Blue Beans` Link: https://codeforces.com/contest/1519/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1519/submission/114564498

---
+++ codeforces/1519/A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=1;i<=n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define nl cout << endl;
+
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    ll r,b,d;
+    cin >> r >> b >>d;
+    ll p ,t;
+    if(r>=b){
+        p = b;
+        t = ceil((dc)r/p);
+        if(d>=(t-1)){
+            yes;
+        }
+        else{
+            no;
+        }
+    }
+    else{
+        p = r;
+        t = ceil((dc)b/p);
+        if(d>=(t-1)){
+            yes;
+        }
+        else{
+            no;
+        }
+    }
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
