8483b21e2f4ef3014931e3344bd9acde3e32222c
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-09 22:17:00 +0530

    Add solution for problem `C1 - k-LCM (easy version)` Link: https://codeforces.com/contest/1497/problem/C1 Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1497/submission/118905267

---
+++ codeforces/1497/C1.cpp
@@ -0,0 +1,73 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=1;i<=n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    int n,k;
+    cin >> n >> k;
+    if(n%2==1){
+        cout << n/2 << " " << n/2 << " " << 1 << endl;
+    }
+    else{
+        int t = n/2;
+        if(t%2==1){
+            cout << t-1 << " " << t-1 << " " << 2 << endl;
+        }
+        else{
+            if(t%2==0){
+                cout << t << " " << t/2 << " " << t/2 << endl; 
+            }
+        }
+    }
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
