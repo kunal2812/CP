8f750d87cd577d9e3e0e5509fcf13f0698ba16cc
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-11 23:10:00 +0530

    Add solution for problem `B - Bear and Finding Criminals` Link: https://codeforces.com/contest/680/problem/B Tags: constructive algorithms, implementation, *1000 Ref: https://codeforces.com/contest/680/submission/119186573

---
+++ codeforces/680/B.cpp
@@ -0,0 +1,78 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=1;i<=n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    int n,a;
+    cin >> n >> a;
+    int arr[102];
+    for(int i=1;i<=n;i++){
+        cin >> arr[i];
+    }
+    int ct=0;
+    for(int i=0;i<n;i++){
+        if(i==0 && arr[a]==1){
+            ct++;
+            continue;
+        }
+        int c = 0;
+        // cout << a-i << " " << a+i << endl;
+        if((a-i)>=1 && arr[a-i]==1){
+            c++;
+        }
+        if((a+i)<=n && arr[a+i]==1){
+            c++;
+        }
+        if(c==2){
+            ct+=2;
+        }
+        if(((a-i)<1 && (a+i)<=n && arr[a+i]==1) || ((a-i)>=1 && (a+i)>n && arr[a-i]==1))ct++;
+    }
+    cout << ct << endl;
+}
+
+int main() {
+    fast;
+        Solution();
+    return 0;
+}
+
