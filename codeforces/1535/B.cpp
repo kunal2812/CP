b4dc281529b00f40d9a4701ed691f0abcdb40c8d
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-13 00:04:00 +0530

    Add solution for problem `B - Array Reodering` Link: https://codeforces.com/contest/1535/problem/B Tags: brute force, greedy, math, number theory, sortings, *900 Ref: https://codeforces.com/contest/1535/submission/119279050

---
+++ codeforces/1535/B.cpp
@@ -0,0 +1,75 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=1;i<=n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+ll mod = 1000000007;
+
+
+// for(int i=0;i<n;i++){
+//     cin >> a[i];
+// }
+
+void Solution(){
+    int n;
+    cin >> n;
+    int a[2100];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    set<pii> s;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) continue;
+            if(__gcd(a[i], 2*a[j])>1){
+                if(s.find(mp(i,j))==s.end() && s.find(mp(j,i))==s.end()){
+                    s.insert(mp(i,j));
+                }
+            }
+        }
+    }
+    cout << s.size() << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
