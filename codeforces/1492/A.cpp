ee2e70995b28144c15bb393bba315be08c947c9f
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-23 20:18:00 +0530

    Add solution for problem `A - Three swimmers` Link: https://codeforces.com/contest/1492/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1492/submission/120376294

---
+++ codeforces/1492/A.cpp
@@ -0,0 +1,61 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN for(int i=1;i<=n;i++){cin >> a[i];}
+#define OUT for(int i=1;i<=n;i++){cout << a[i] << " ";}
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+ll mod = 1000000007;
+
+
+
+void Solution(){
+    ll p,a,b,c;
+    cin >> p >> a >> b >> c;
+    ll ans = min(ceil((dc)p/a)*a-p, min(ceil((dc)p/b)*b-p,ceil((dc)p/c)*c-p));
+    cout << ans << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
