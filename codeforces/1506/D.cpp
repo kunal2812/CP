8ff8f8ba39bf9724dec0ebfa1e7b42437d3b72aa
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-28 15:19:00 +0530

    Add solution for problem `D - Epic Transformation` Link: https://codeforces.com/contest/1506/problem/D Tags: constructive algorithms, data structures, greedy, *1400 Ref: https://codeforces.com/contest/1506/submission/120790580

--- codeforces/1506/D.cpp
+++ codeforces/1506/D.cpp
@@ -9,25 +9,10 @@ using namespace std;
 #define sc second
 #define endl '\n'
 
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
 void Solution(){ 
     int n;
     cin >> n;
-    unordered_map<int, int, custom_hash> m;
+    map<int, int> m;
     int x;
     for(int i=0;i<n;i++){
         cin >> x;
