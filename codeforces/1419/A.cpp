11e4bdff915fa1ef8fec784fbb2d694b99211de9
kunal2812 <kunalkatiyar50@gmail.com>
2021-06-30 23:11:00 +0530

    Add solution for problem `A - Digit Game` Link: https://codeforces.com/contest/1419/problem/A Tags: games, greedy, implementation, *900 Ref: https://codeforces.com/contest/1419/submission/120972325

---
+++ codeforces/1419/A.cpp
@@ -0,0 +1,125 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+ll mod = 1000000007;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+void Solution(){ 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    list<int> raze; list<int> breach;
+    for(int i=0;i<n;i+=2){
+        if((s[i]-'0')%2==0){
+            raze.pb(s[i]-'0');
+        }
+    }
+    for(int i=0;i<n;i+=2){
+        if((s[i]-'0')%2==1){
+            raze.pb(s[i]-'0');
+        }
+    }
+    for(int i=1;i<n;i+=2){
+        if((s[i]-'0')%2==1){
+            breach.pb(s[i]-'0');
+        }
+    }
+    for(int i=1;i<n;i+=2){
+        if((s[i]-'0')%2==0){
+            breach.pb(s[i]-'0');
+        }
+    }
+    int i=0;
+    while((raze.size()+breach.size())>1){
+        if(i%2==0){
+            raze.pop_front();
+        }
+        else{
+            breach.pop_front();
+        }
+        i++;
+    } 
+    int t;
+    if(raze.size()!=0){
+        t = raze.front();
+    }
+    if(breach.size()!=0){
+        t = breach.front();
+    }
+    if(t%2==0){
+        cout << 2;
+    }
+    else{
+        cout << 1;
+    }nl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
