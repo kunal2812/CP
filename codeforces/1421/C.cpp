63b91153308f61bb9fdb35425f7c0fce9cd87be8
kunal2812 <kunalkatiyar50@gmail.com>
2021-07-22 00:49:00 +0530

    Add solution for problem `C - Palindromifier` Link: https://codeforces.com/contest/1421/problem/C Tags: constructive algorithms, strings, *1400 Ref: https://codeforces.com/contest/1421/submission/123220194

---
+++ codeforces/1421/C.cpp
@@ -0,0 +1,76 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+
+
+ll mod = 1000000007;
+
+
+
+
+void Solution(){
+    string s; cin >> s;
+    int n = s.length();
+    cout << 3 << endl;
+    cout << "L " << 2 << endl;
+    cout << "R " << 2 << endl;
+    cout << "R " << 2*n-1 << endl;
+}
+
+int main() {
+    fast;
+    // int T;
+    // cin >> T;
+    // while(T!=0){
+        Solution();
+    //     T--;
+    // }
+    return 0;
+}
+
