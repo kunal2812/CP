c6fb747625458d60700e091fc6125f27b9f7ca3a
kunal2812 <kunalkatiyar50@gmail.com>
2021-07-27 23:53:00 +0530

    Add solution for problem `C - Penalty` Link: https://codeforces.com/contest/1553/problem/C Tags: bitmasks, brute force, dp, greedy, *1200 Ref: https://codeforces.com/contest/1553/submission/123964858

---
+++ codeforces/1553/C.cpp
@@ -0,0 +1,145 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+// To rotate an array
+// rotate(v.begin(), v.end()+k, v.end())
+
+// To fill consecutive values in a vector
+// iota(all(v),1);
+
+
+ll mod = 1000000007;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+void Solution(){ 
+    string s; cin >> s;
+    int a=0; int b=0;
+    int t1=5,t2=5;
+    int ans = 0;
+    for(int i=0;i<10;i++){
+        if(i%2==0){
+            if(s[i]=='?'){
+                a+=1;
+            }
+            else if(s[i]!='?'){
+                a+=(s[i]-'0');
+            }
+            t1--;
+        }
+        else{
+            if(s[i]!='?'){
+                b+=(s[i]-'0');
+            }
+            t2--;
+        }
+        if(a>(b+t2) || b>(a+t1)){
+            // cout << i+1 << endl;
+            ans = i+1;
+            break;
+        }
+    }
+    if(ans==0){
+        ans=10;
+    }
+    t1=5,t2=5; a=0, b=0;
+    for(int i=0;i<10;i++){
+        if(i%2==0){
+            if(s[i]!='?'){
+                a+=(s[i]-'0');
+            }
+            t1--;
+        }
+        else{
+            if(s[i]=='?'){
+                b+=1;
+            }
+            if(s[i]!='?'){
+                b+=(s[i]-'0');
+            }
+            t2--;
+        }
+        if(a>(b+t2) || b>(a+t1)){
+            // cout << i+1 << endl;
+            ans = min(ans,i+1);
+            break;
+        }
+    }
+    if(ans==0){
+        ans = 10;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
