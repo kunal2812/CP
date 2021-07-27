5c60e0e8b19e0a0fc6d326855bdcf74405addb39
kunal2812 <kunalkatiyar50@gmail.com>
2021-07-27 18:07:00 +0530

    Add solution for problem `D2 - Domino (hard version)` Link: https://codeforces.com/contest/1551/problem/D2 Tags: constructive algorithms, implementation, math, *2100 Ref: https://codeforces.com/contest/1551/submission/123934594

---
+++ codeforces/1551/D2.cpp
@@ -0,0 +1,257 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+// To rotate an array
+// rotate(v.begin(), v.end()+k, v.end())
+
+// To fill consecutive values in a vector
+// iota(all(v),1);
+
+
+ll mod = 1000000007;
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+
+char ans[102][102];
+
+int up(int i, int j, int n, int m, char curr){
+    hell:;
+    int t = i*3 + j*9;
+    t%=26;
+    curr = 'a'+t;
+    if(curr>'z'){
+        curr = 'a';
+        goto hell;
+    }
+    return curr;
+}
+
+
+void Solution(){ 
+    int n,m,k; cin >> n >> m >> k ;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            ans[i][j]=0;
+        }
+    }
+    int kkk = k;
+    int t = n*m/2;
+    char curr = 'a';
+    if(n%2==0 && m%2==0){
+        if(k%2==0){
+            cout << "YES" << endl;
+            for(int j=0;j<m;j+=2){
+                for(int i=0;i<n;i++){
+                    if(k>0){
+                        if(curr>'z'){
+                            curr = 'a';
+                        }
+                        curr = up(i,j,n ,m, curr);
+                        ans[i][j] = curr;
+                        ans[i][j+1] = curr;
+                        k--; curr++;
+                    }
+                    else{
+                        goto hell;
+                    }
+                }
+            }
+            hell:;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(ans[i][j]==0){
+                        if(curr>'z'){
+                            curr = 'a';
+                        }
+                        curr = up(i,j,n ,m, curr);
+                        ans[i][j] = curr;
+                        ans[i+1][j] = curr;
+                        curr++;
+                    }
+                }
+            }
+        }
+        else{
+            no;
+        }
+    }
+    else if(n%2==0 && m%2==1){
+        int l = t - (n/2);
+        int jj;
+        for(jj=l;jj>=0;jj-=2){
+            if(jj==k){
+                cout << "YES" << endl;
+                for(int j=0;j<m;j+=2){
+                    for(int i=0;i<n;i++){
+                        if(k>0){
+                            if(curr>'z'){
+                                curr = 'a';
+                            }
+                            curr = up(i,j,n ,m, curr);
+                            ans[i][j] = curr;
+                            ans[i][j+1] = curr;
+                            k--; curr++;
+                        }
+                        else{
+                            goto hell2;
+                        }
+                    }
+                }
+                hell2:;
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<m;j++){
+                        if(ans[i][j]==0){
+                            if(curr>'z'){
+                                curr = 'a';
+                            }
+                            curr = up(i,j,n ,m, curr);
+                            ans[i][j] = curr;
+                            ans[i+1][j] = curr;
+                            curr++;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        if(jj!=kkk){
+            no;
+        }
+    }
+    else if(n%2==1 && m%2==0){
+        int l = m/2;
+        int jj;
+        for(jj=l;jj<=k;jj+=2){
+            if(jj==k){
+                cout << "YES" << endl;
+                for(int i=0;i<m;i+=2){
+                        if(k>0){
+                            if(curr>'z'){
+                                curr = 'a';
+                            }
+                            curr = up(0,i,n ,m, curr);
+                            ans[0][i] = curr;
+                            ans[0][i+1] = curr;
+                            k--; curr++;
+                        }
+                        else{
+                            break;
+                        }
+                }
+                for(int j=0;j<m;j+=2){
+                    for(int i=1;i<n;i++){
+                        if(k>0){
+                            if(curr>'z'){
+                                curr = 'a';
+                            }
+                            curr = up(i,j,n ,m, curr);
+                            ans[i][j] = curr;
+                            ans[i][j+1] = curr;
+                            k--; curr++;
+                        }
+                        else{
+                            goto hell3;
+                        }
+                    }
+                }
+                hell3:;
+                for(int i=1;i<n;i++){
+                    for(int j=0;j<m;j++){
+                        if(ans[i][j]==0){
+                            if(curr>'z'){
+                                curr = 'a';
+                            }
+                            curr = up(i,j,n ,m, curr);
+                            ans[i][j] = curr;
+                            ans[i+1][j] = curr;
+                            curr++;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        if(jj!=kkk){
+            no;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout << ans[i][j];
+        }
+        nl;
+    }
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
