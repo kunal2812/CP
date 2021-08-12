9fc8afa5b3bfeddcbf172728982d9e7455d67142
kunal2812 <kunalkatiyar50@gmail.com>
2021-08-12 22:11:00 +0530

    Add solution for problem `E - Advertising Agency` Link: https://codeforces.com/contest/1475/problem/E Tags: combinatorics, math, sortings, *1600 Ref: https://codeforces.com/contest/1475/submission/125690645

---
+++ codeforces/1475/E.cpp
@@ -0,0 +1,114 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+
+ll mod = 1000000007;
+
+unsigned long long power(unsigned long long x,
+                                  int y, int p)
+{
+    unsigned long long res = 1;
+    x = x % p;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+unsigned long long modInverse(unsigned long long n, int p)
+{
+    return power(n, p - 2, p);
+}
+unsigned long long nCrModPFermat(unsigned long long n, int r, int p)
+{
+    if (n < r)
+        return 0;
+    if (r == 0)
+        return 1;
+    unsigned long long fac[n + 1];
+    fac[0] = 1;
+    for (int i = 1; i <= n; i++)
+        fac[i] = (fac[i - 1] * i) % p;
+ 
+    return (fac[n] * modInverse(fac[r], p) % p * modInverse(fac[n - r], p) % p) % p;
+}
+void Solution(){ 
+    int n,k; cin >> n >> k;
+    vi a(n);
+    umii m; ll ans=1;
+    for(int i=0;i<n;i++){
+        cin >> a[i]; m[a[i]]++;
+    }
+    sortd(a);
+    int t = m[a[k-1]]; int r = t;
+    for(int i=0;i<k;i++){
+        if(a[i]==a[k-1]){
+            r--;
+        }
+    }
+    // cout << t <<  " "<< r << endl;
+    ans = nCrModPFermat(t,r,mod);
+    cout << ans << endl;
+}
+
+int main() {
+    fast;
+    int T;
+    cin >> T;
+    while(T!=0){
+        Solution();
+        T--;
+    }
+    return 0;
+}
+
