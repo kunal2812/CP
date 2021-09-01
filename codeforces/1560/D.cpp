01489c06c92b7cbeaf6d007abba5589622441df5
kunal2812 <kunalkatiyar50@gmail.com>
2021-09-01 17:40:00 +0530

    Add solution for problem `D - Make a Power of Two` Link: https://codeforces.com/contest/1560/problem/D Tags: greedy, math, strings, *1300 Ref: https://codeforces.com/contest/1560/submission/127598835

---
+++ codeforces/1560/D.cpp
@@ -0,0 +1,112 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return;
+#define no  cout << "NO" << endl; return;
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl;
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+// To rotate an array
+// rotate(v.begin(), v.end()+k, v.end())
+
+// To fill consecutive values in a vector
+// iota(all(v),1);
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+ll mod = 1000000007;
+
+ll temp[65];
+vector<string> pow2(62);
+
+void init(){
+	temp[0] = 1;
+	pow2[0] = "1";
+	for(int i=1;i<=55;i++){
+		temp[i] = temp[i-1]*2;
+		pow2[i] = to_string(temp[i]);
+	}
+}
+int ops(string s, string t){
+	int n = sz(s); int m = sz(t);
+	int j=0;
+	for(int i=0;i<n;i++){
+		if(j<m && s[i]==t[j]){
+			j++;
+		}
+	}
+	return (m-j+n-j);
+}
+
+void Solution(int x){ 
+    string n; cin >> n; int ans = INT_MAX;
+    for(int i=0;i<=55;i++){
+    	ans = min(ans, ops(n, pow2[i]));
+    }
+    pr(ans); pr(endl);
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    fast;
+    init();
+    int T;
+    cin >> T;
+    int i=0;
+    while(T!=0){
+        Solution(++i);
+        T--;
+    }
+    return 0;
+}
+
+
+
