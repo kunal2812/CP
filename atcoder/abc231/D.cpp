d35ee97b8ab1d0d5cd5370f91f2990a6fecc51bf
kunal2812 <kunalkatiyar50@gmail.com>
2021-12-11 18:07:00 +0530

    Add solution for problem `D - Neighbors` Link: https://atcoder.jp/contests/abc231/tasks/abc231_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc231/submissions/27836736

---
+++ atcoder/abc231/D.cpp
@@ -0,0 +1,125 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+#define nf string::npos
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef map<char,int> mci;
+typedef map<string,int> msi;
+typedef pair<int, int> pii;
+typedef unordered_map<char, int> umci;
+typedef unordered_map<string, int> umsi;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<int, vi> umiv;
+// m.reserve(1024);
+// m.max_load_factor(0.25);
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usll;
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+
+#define printA(a) for(auto x:a){ cout << x << " "; }nl;
+// To rotate an array
+// rotate(v.begin(), v.end()+k, v.end())
+
+// To fill consecutive values in a vector
+// iota(all(v),1);
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+vi graph[100005];
+bool visited[100005];
+bool flag = false;
+
+void dfs(int node, int p){
+    visited[node] = true;
+    for(int x:graph[node]){
+        if(visited[x] && x!=p){
+            flag=true;
+            return;
+        }
+        else if(visited[x]){
+            continue;
+        }
+        else{
+            dfs(x, node);
+        }
+    }
+}
+
+void Solution(int xx){ 
+    int n,m; cin >> n >> m;
+    vi check(n+1,0);
+    fr(i,0,m,1){
+        int a , b; cin >> a >> b;
+        check[a]++;check[b]++;
+        if(check[a]>2 || check[b]>2){
+            pr("No"); return;
+        }
+        graph[a].pb(b); graph[b].pb(a);
+    }
+    fr(i,1,n+1,1){
+        if(!visited[i]){
+            dfs(i,-1);
+        }
+    }
+    if(!flag){
+        pr("Yes");
+    }
+    else{
+        pr("No");
+    }
+}
+
+int main() {
+    fast;
+    // int T;
+    // cin >> T;
+    int i=0;
+    // while(T!=0){
+        Solution(++i);
+    //     T--;
+    // }
+    return 0;
+}
