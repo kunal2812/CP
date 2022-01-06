aaa3b0459e3b2f94c2511b663b881d095670887d
kunal2812 <kunalkatiyar50@gmail.com>
2022-01-06 19:31:00 +0530

    Add solution for problem `E - Replace the Numbers` Link: https://codeforces.com/contest/1620/problem/E Tags: constructive algorithms, data structures, dsu, implementation, *1900 Ref: https://codeforces.com/contest/1620/submission/141825807

--- codeforces/1620/E.cpp
+++ codeforces/1620/E.cpp
@@ -1,104 +1,102 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("O3")
-#pragma GCC target("popcnt")
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define ll long long
-#define mp make_pair
 #define pb push_back
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-#define yes cout << "YES" << endl; return
-#define no  cout << "NO" << endl; return
-#define dc double
 #define ff first
 #define sc second
 #define endl '\n'
 #define nl cout << endl
 
-#define sorta(v) sort(v.begin(), v.end())
-#define sortd(v) sort(v.begin(), v.end(), greater<int>())
-#define rev(s) reverse(s.begin(), s.end())
-
 typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef map<int,int> mii;
-typedef map<char,int> mci;
-typedef map<string,int> msi;
 typedef pair<int, int> pii;
-typedef unordered_map<char, int> umci;
-typedef unordered_map<string, int> umsi;
-typedef unordered_map<int, int> umii;
-// m.reserve(1024);
-// m.max_load_factor(0.25);
-typedef unordered_set<int> usi;
-typedef unordered_set<ll> usll;
-typedef priority_queue<int> pqmax;
-typedef priority_queue<int, vector<int>, greater<int>> pqmin;
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define sz(a) a.size()
-#define pr(x) cout << x << endl
-
-#define ctset(n) __builtin_popcountll(n)
-#define ctunset(n) __builtin_ctzll(x)
-#define gtmax(a) *max_element(a.begin(), a.end())
-
-#define printA(a) for(auto x:a){ cout << x << " "; }nl;
-// To rotate an array
-// rotate(v.begin(), v.end()+k, v.end())
-
-// To fill consecutive values in a vector
-// iota(all(v),1);
-
-// For problem involving precision
-// cout << fixed << setprecision(10) << xx+mx << endl;
 
 #define fr(i, a, b, c) for(int i=a; i<b; i+=c)
 #define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
 
-ll mod = 1000000007;
+int cnt = 0;
+unordered_map<int,int> tab;
+map<int,int> parent;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int find(int x){
+    if(parent[x]==x){
+        return x;
+    }
+    return parent[x] = find(parent[x]);
+}
+void merge(int x, int y){
+    if(tab.find(x)==tab.end() || x==y){
+        return;
+    }
+    if(tab.find(y)==tab.end()){
+        int p = tab[x];
+        tab[y] = p;
+        tab.erase(x);
+        return;
+    }
+    int q = x;
+    x = find(tab[x]);
+    y = find(tab[y]);
+    if(x!=y){
+        parent[x] = y; 
+    }
+    tab.erase(q);
+}
+void make(int x){
+    if(tab.find(x)==tab.end()){
+        parent[cnt] = cnt;
+        tab[x] = cnt;
+        cnt++;
+    }
+    else{
+        parent[cnt] = tab[x];
+        cnt++;
+    }
+}
+void print(){
+    unordered_map<int, int> m;
+    // cout << "PARENT" << endl;
+    for(auto t:parent){
+        int x = t.ff, y = t.sc;
+        parent[x] = find(y);
+    //     // cout << x.ff << " " << x.sc << endl;
+    }
+    // cout << "TAB" << endl;
+    for(auto x:tab){
+        // cout << x.ff << " " << x.sc << endl;
+        m[x.sc] = x.ff;
+    }
+    // nl;
+    // cout << "ARRAY" << endl;
+    for(pii x:parent){
+        cout << m[x.sc] << " ";
+    }nl;
+}
 
-int cnt=0;
-int ans[500005];
-map<int, list<int>> m;
-void Solution(int xx){ 
+void Solution(){ 
     int q; cin >> q;
-    int x,a,b; 
+    int x,a,b;
     while(q--){
-        cin >> x;
+        cin >> x; 
         if(x==1){
             cin >> a;
-            m[a].pb(cnt);
-            cnt++;
+            make(a);
         }
         else{
             cin >> a >> b;
-            m[b].splice(m[b].end(), m[a]);
+            merge(a,b);
+            
         }
+        // print();
     }
-    for(auto x:m){
-        for(auto itr = x.sc.begin(); itr!=x.sc.end(); itr++){
-            ans[*itr] = x.ff;
-        }
-    }
-    for(int i=0; i<cnt; i++){
-        cout << ans[i] << " ";
-    }nl;
+    print();
 }
 
 int main() {
     fast;
-    int t, i=0;
-    // cin >> t;
-    // while(t--){
-        Solution(++i);
-    // }
+    Solution();
     return 0;
 }
