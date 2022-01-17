7076c1801682232c474cbe77130aad3953827227
kunal2812 <kunalkatiyar50@gmail.com>
2022-01-17 21:54:00 +0530

    Add solution for problem `A - Strange Birthday Party` Link: https://codeforces.com/contest/1470/problem/A Tags: binary search, dp, greedy, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1470/submission/143144364

---
+++ codeforces/1470/A.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umii;
+
+typedef unordered_set<int> usi;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+#define gtmin(a) *min_element(a.begin(), a.end())
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+void solve(int xx){ 
+    int n,m; cin >> n >> m;
+    vector<pii> k(n);
+    list<int> c; vi d(m);
+    fr(i,0,n,1){
+        cin >> k[i].sc;
+    }
+    fr(j,0,m,1){
+        int t;
+        cin >> d[j]; c.push_back(d[j]);
+    } 
+    ll ans = 0;
+    fr(i,0,n,1){
+        k[i].ff = d[k[i].sc-1];
+    }
+    sort(k.begin(), k.end(), greater<pii>());
+    vector<bool> vis(m);
+    fr(i,0,n,1){
+        if(!c.empty() && c.front()<k[i].ff){
+            ans+=c.front(); c.pop_front();
+        }
+        else{
+            ans+=k[i].ff;
+        }
+    }
+    pr(ans);
+}
+
+int main() {
+    fast;
+    int t, i=0; cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
