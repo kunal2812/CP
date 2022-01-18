fd9d7b41393ef301a9181104791bc7998c4d3823
kunal2812 <kunalkatiyar50@gmail.com>
2022-01-18 17:41:00 +0530

    Add solution for problem `D - Zero Remainder Array` Link: https://codeforces.com/contest/1374/problem/D Tags: math, sortings, two pointers, *1400 Ref: https://codeforces.com/contest/1374/submission/143219907

---
+++ codeforces/1374/D.cpp
@@ -0,0 +1,79 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef map<int,int> mii;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umii;
+
+typedef unordered_set<int> usi;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+#define gtmax(a) *max_element(a.begin(), a.end())
+#define gtmin(a) *min_element(a.begin(), a.end())
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+/*
+1 1 2 3
+2 5 1 0
+*/
+void solve(int xx){ 
+    int n,k; cin >> n >> k;
+    vi a(n); mii m;
+    fr(i,0,n,1){
+        int t; cin >> t; m[t%k]++;
+    } m.erase(0); if(m.empty()){
+        pr(0); return;
+    }
+    ll ans=0;
+    for(auto p:m){
+        ans = max(ans, (ll)(k-p.ff)+(ll)k*(p.sc-1));
+    }
+    pr(ans+1);
+}
+
+int main() {
+    fast;
+    int t, i=0; cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
