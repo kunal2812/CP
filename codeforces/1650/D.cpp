db4331709f7a66f817bad840fd30bcb113e03e18
kunal2812 <kunalkatiyar50@gmail.com>
2022-03-12 21:51:00 +0530

    Add solution for problem `D - Twist the Permutation` Link: https://codeforces.com/contest/1650/problem/D Tags: brute force, constructive algorithms, implementation, math, *1300 Ref: https://codeforces.com/contest/1650/submission/149444341

---
+++ codeforces/1650/D.cpp
@@ -0,0 +1,88 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+1 2 3 4 5 6
+      1
+3 2 5 6 1 4
+
+*/
+
+void rot(vi &a, int n){
+    int x = a[0];
+    fr(i,0,n-1,1){
+        a[i] = a[i+1];
+    }
+    a[n-1] = x;
+}
+
+void solve(int xx){ 
+    int n; cin >> n;
+    vi a(n), ans(n,0);
+    fr(i,0,n,1){
+        cin >> a[i];
+    }
+    rfr(i,n-1,0,1){
+        while(a[i]!=(i+1)){
+            rot(a,i+1);
+            ans[i]++;
+        }
+    }
+    for(auto x:ans){
+        cout << x << " ";
+    }nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
