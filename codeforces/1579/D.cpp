0ac7a51903411e90791c27b04519c28f4dd261d3
kunal2812 <kunalkatiyar50@gmail.com>
2022-03-12 12:22:00 +0530

    Add solution for problem `D - Productive Meeting` Link: https://codeforces.com/contest/1579/problem/D Tags: constructive algorithms, graphs, greedy, *1400 Ref: https://codeforces.com/contest/1579/submission/149387750

---
+++ codeforces/1579/D.cpp
@@ -0,0 +1,90 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    int n; cin >> n;
+    priority_queue<pii> pq; int x;
+    fr(i,0,n,1){
+        cin >> x;
+        if(x>0){
+            pq.push({x,i+1});
+        }
+    }
+    if(sz(pq)<=1){
+        pr(0); return;
+    }
+    vector<pii> ans;
+    while(sz(pq)>1){
+        pii a = pq.top(); pq.pop();
+        pii b = pq.top(); pq.pop();
+        ans.pb({a.sc,b.sc});
+        a.ff--,b.ff--;
+        if(a.ff!=0){
+            pq.push(a);
+        }
+        if(b.ff!=0){
+            pq.push(b);
+        }
+    }
+    cout << sz(ans) << endl;
+    for(auto x:ans){
+        cout << min(x.ff,x.sc) << " " << max(x.ff,x.sc) << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
