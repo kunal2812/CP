0b1cfcf11d01647835db0f8e71ac9392cd0df360
kunal2812 <kunalkatiyar50@gmail.com>
2022-03-12 17:39:00 +0530

    Add solution for problem `B - Hit and Blow` Link: https://atcoder.jp/contests/abc243/tasks/abc243_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc243/submissions/30034793

---
+++ atcoder/abc243/B.cpp
@@ -0,0 +1,83 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    int n; cin >> n;
+    vi a(n), b(n);
+    unordered_map<int, int> m; int cnt2=0;
+    fr(i,0,n,1){
+        cin >> a[i];
+        m[a[i]] = i;
+    } 
+    fr(i,0,n,1){ cin >> b[i];
+        if(m.find(b[i])!=m.end()){
+            if(m[b[i]]!=i){
+                cnt2++;
+            }
+        }
+    }
+    int cnt1 = 0;
+    fr(i,0,n,1){
+        if(a[i]==b[i]){
+            cnt1++;
+        }
+    }
+    cout << cnt1 << endl << cnt2 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
+}
