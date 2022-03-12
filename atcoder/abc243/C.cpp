0294a9f1c97813227cf50a5122e4cb4686d910b6
kunal2812 <kunalkatiyar50@gmail.com>
2022-03-12 20:01:00 +0530

    Add solution for problem `C - Collision 2` Link: https://atcoder.jp/contests/abc243/tasks/abc243_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc243/submissions/30084121

---
+++ atcoder/abc243/C.cpp
@@ -0,0 +1,90 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    int n; cin >> n;
+    unordered_map<int, vector<pii>> y;
+    int u,v;
+    fr(i,0,n,1){
+        cin >> u >> v;
+        y[v].pb({u,i});
+    }
+    string s; cin >> s;
+    for(auto x:y){
+        int cntl = 0;
+        int cntr = 0;
+        int minl = INT_MAX;
+        int maxr = INT_MIN;
+        for(auto t:x.sc){
+            if(s[t.sc]=='R'){
+                minl = min(minl, t.ff);
+                cntr++;
+            }
+            else{
+                maxr = max(maxr, t.ff);
+                cntl++;
+            }
+        }
+        if(cntl>=1 && cntr>=1 && minl<maxr){
+            cout << "Yes" << endl; return;
+        }
+    }
+    cout << "No" << endl; return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
+}
