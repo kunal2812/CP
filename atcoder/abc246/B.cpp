f51f2127a10078472d0d6fd5d1852b277ee4d0c1
kunal2812 <kunalkatiyar50@gmail.com>
2022-04-02 18:00:00 +0530

    Add solution for problem `B - Get Closer` Link: https://atcoder.jp/contests/abc246/tasks/abc246_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc246/submissions/30645830

---
+++ atcoder/abc246/B.cpp
@@ -0,0 +1,70 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    long double a,b; cin >> a >> b;
+    if(a==0){
+        cout << 0 << " " << 1 << endl; return;
+    }
+    long double m = (long double)b/a;
+    long double x = (long double)1/sqrt((1+(long double)m*m));
+    long double y = (long double)m*x;
+    cout << fixed << setprecision(12) << x << " " << y <<  endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
+}
