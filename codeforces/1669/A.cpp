213225dd3b7d28d3ff8ec0603e7a480cb73723f9
kunal2812 <kunalkatiyar50@gmail.com>
2022-04-27 10:36:00 +0530

    Add solution for problem `A - Division?` Link: https://codeforces.com/contest/1669/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1669/submission/155088877

---
+++ codeforces/1669/A.cpp
@@ -0,0 +1,76 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    ll r; cin >> r;
+    cout << "Division" << " ";
+    if(r>=1900){
+        pr(1);
+    }
+    else if(r>=1600 && r<1900){
+        pr(2);
+    }
+    else if(1400<=r && r<=1599){
+        pr(3);
+    }
+    else{
+        pr(4);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
