5e151388e3bdb29443114c22597db16967b093e8
kunal2812 <kunalkatiyar50@gmail.com>
2022-05-02 20:12:00 +0530

    Add solution for problem `A - Number Transformation` Link: https://codeforces.com/contest/1674/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1674/submission/155599112

---
+++ codeforces/1674/A.cpp
@@ -0,0 +1,71 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    ll x,y; cin >> x >> y;
+    fr(i,1,10,1){
+        fr(j,1,100,1){
+            if(((ll)pow(j,i)*x)==y){
+                cout << i << " " << j << endl; return;
+            }
+        }
+    }
+    cout << 0 << " " << 0 << endl; return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
