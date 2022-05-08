92de238aa27acd08760ac77ccb8c9c00f136a414
kunal2812 <kunalkatiyar50@gmail.com>
2022-05-08 20:28:00 +0530

    Add solution for problem `A - Tokitsukaze and All Zero Sequence` Link: https://codeforces.com/contest/1678/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1678/submission/156312407

---
+++ codeforces/1678/A.cpp
@@ -0,0 +1,89 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    int n; cin >> n;
+    vi a(n); ll ans = 0;
+    bool flag = false; bool duplicate = false;
+    multiset<int> s;
+    fr(i,0,n,1){
+        cin >> a[i];
+        if(a[i]==0){
+            flag = true;
+        }
+        if(a[i]==0){
+            continue;
+        }
+        if(s.find(a[i])!=s.end()){
+            duplicate = true;
+        }
+        s.insert(a[i]);
+    }
+    if(flag){
+        ans+=sz(s);
+    }
+    else if(duplicate){
+        ans+=sz(s);
+    }
+    else{
+        ans=sz(s)+1;
+    }
+    pr(ans);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
