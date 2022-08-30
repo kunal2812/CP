844d258aa036d51eb09b7ec8c9c5032b2cb8b5c0
kunal2812 <kunalkatiyar50@gmail.com>
2022-08-30 20:53:00 +0530

    Add solution for problem `D - Line` Link: https://codeforces.com/contest/1722/problem/D Tags: greedy, sortings, *1100 Ref: https://codeforces.com/contest/1722/submission/170226846

---
+++ codeforces/1722/D.cpp
@@ -0,0 +1,95 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os; // find_by_order, order_of_key
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> oms; // ordered_multiset
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+void solve(int xx){ 
+    int n; cin >> n;
+    string s; cin >> s;
+    ll ans = 0;
+    vll a(n,0);
+    vll b(n,0);
+    fr(i,0,n,1){
+        if(s[i]=='L'){
+            a[i] = i;
+            b[i] = n-1-i;
+        }
+        else{
+            a[i] = n-1-i;
+            b[i] = i;
+        }
+        ans+=a[i];
+    }
+    fr(i,0,n,1){
+        b[i]-=a[i];
+    }
+    sortd(b);
+    fr(i,1,n+1,1){
+        if(b[i-1]>0){
+            ans+=b[i-1];
+        }
+        cout << ans << " ";
+    }nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
