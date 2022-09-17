6f62dba492eb8b13c3a7d546cf0d17ab997dc50a
kunal2812 <kunalkatiyar50@gmail.com>
2022-09-17 18:46:00 +0530

    Add solution for problem `C - Submask` Link: https://atcoder.jp/contests/abc269/tasks/abc269_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc269/submissions/34947427

---
+++ atcoder/abc269/C.cpp
@@ -0,0 +1,89 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os; // find_by_order, order_of_key
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> oms; // ordered_multiset
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(ll i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(ll i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+set<ll> s;
+
+
+void solve(int x){
+    ll n; cin >> n;
+    vll sb;
+    fr(i,0,60,1){
+        if((ll)n&(ll)((ll)1<<i)){
+            sb.pb(i);
+        }
+    }
+    // cout << sb.size() << endl;
+    for(ll i = 0; i<(1<<(ll)sb.size()); i++){
+        ll pp = 0;
+        for(ll j=0;j<sb.size();j++){
+            if((ll)((ll)i&((ll)1<<j))){
+                pp+=(ll)((ll)1<<(ll)sb[j]);
+            }
+        }
+        s.insert((ll)pp);
+    }
+    for(auto x:s){
+        cout << x << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
+}
