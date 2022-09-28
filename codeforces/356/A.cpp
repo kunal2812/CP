ec93222a62f7e6b580ac211e77be73a54bebfcb4
kunal2812 <kunalkatiyar50@gmail.com>
2022-09-28 12:06:00 +0530

    Add solution for problem `A - Knight Tournament` Link: https://codeforces.com/contest/356/problem/A Tags: data structures, dsu, *1500 Ref: https://codeforces.com/contest/356/submission/173781043

---
+++ codeforces/356/A.cpp
@@ -0,0 +1,100 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvl;
+typedef map<int,int> mii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os; // find_by_order, order_of_key
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> oms; // ordered_multiset
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+#define pr(x) cout << x << endl
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+ll mod = 1000000007;
+
+/*
+
+*/
+int par[300005];
+
+
+void solve(int xx){ 
+    int n,m; cin >> n >> m;
+    set<int> s;
+    fr(i,1,n+1,1) s.insert(i);
+    while(m--){
+        int l,r,x; cin >> l >> r >> x;
+        vi del;
+        for(auto itr=s.lower_bound(l);itr!=s.end(); itr++){
+            if(*itr>r){
+                break;
+            }
+            if(*itr!=x){
+                par[*itr] = x;
+                del.pb(*itr);
+            }
+        }
+        for(auto x:del){
+            s.erase(x);
+        }
+    }
+    fr(i,1,n+1,1){
+        cout << par[i] << " ";
+    }nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
+}
