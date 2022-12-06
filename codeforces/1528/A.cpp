2b8d34af2368a4971fbe69728380961fe86bf4ed
Kunal Katiyar <kunalkatiyar50@gmail.com>
2022-12-06 22:52:00 +0530

    Add solution for problem `A - Parsa's Humongous Tree` Link: https://codeforces.com/contest/1528/problem/A Tags: dfs and similar, divide and conquer, dp, greedy, trees, *1600 Ref: https://codeforces.com/contest/1528/submission/184127400

---
+++ codeforces/1528/A.cpp
@@ -0,0 +1,103 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvl;
+typedef map<int,int> mii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os; // find_by_order, order_of_key (#less than key)
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> oms; // ordered_multiset
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+#define scn(x) cin >> x
+#define scna(a) for(int i=0; i<n; i++){cin >> a[i];}
+#define pr(x) cout << x << endl
+#define pra(a) for(auto x:a){cout << x << " ";}nl
+
+ll mod = 1000000007;
+
+/*
+
+*/
+pii val[100005];
+vi tr[100005];
+ll dp[100005][2];
+void dfs(int x, int p){
+    for(auto u:tr[x]){
+        if(u==p){
+            continue;
+        }
+        dfs(u,x);
+        dp[x][1]+=max(dp[u][1]+abs(val[u].sc-val[x].sc),dp[u][0]+abs(val[u].ff-val[x].sc));
+        dp[x][0]+=max(dp[u][1]+abs(val[u].sc-val[x].ff),dp[u][0]+abs(val[u].ff-val[x].ff));
+    }
+}
+void solve(int xx){ 
+    int n; cin >> n;
+    fr(i,1,n+1,1){
+        tr[i].clear(); dp[i][0] = dp[i][1] = 0;
+        cin >> val[i].ff >> val[i].sc;
+    }
+    fr(i,1,n,1){
+        int u,v; cin >> u >> v;
+        tr[u].pb(v); tr[v].pb(u);
+    }
+    dfs(1,-1);
+    pr(max(dp[1][0], dp[1][1]));
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    cin >> t;
+    while(t--)  
+        solve(++i);
+    return 0;
+}
