280539634b276809028adeadc32c8a54528ef60d
Kunal Katiyar <kunalkatiyar50@gmail.com>
2023-02-14 23:13:00 +0530

    Add solution for problem `E - A and B and Lecture Rooms` Link: https://codeforces.com/contest/519/problem/E Tags: binary search, data structures, dfs and similar, dp, trees, *2100 Ref: https://codeforces.com/contest/519/submission/193636429

--- codeforces/519/E.cpp
+++ codeforces/519/E.cpp
@@ -1,106 +1,203 @@
-#include<bits/stdc++.h>
+#pragma GCC target ("avx2")
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#include <bits/stdc++.h>
 using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+
+#define yes cout << "YES" << endl; return
+#define no  cout << "NO" << endl; return
+
+#define dc double
+#define ff first
+#define sc second
+#define endl '\n'
+#define nl cout << endl
+
+#define sorta(v) sort(v.begin(), v.end())
+#define sortd(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(s) reverse(s.begin(), s.end())
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvl;
+typedef map<int,int> mii;
+
+typedef priority_queue<int> pqmax;
+typedef priority_queue<int, vector<int>, greater<int>> pqmin;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os; // find_by_order, order_of_key (#less than key)
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> oms; // ordered_multiset
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(a) a.size()
+
+#define ctset(n) __builtin_popcountll(n)
+#define ctunset(n) __builtin_ctzll(x)
+
+// For problem involving precision
+// cout << fixed << setprecision(10) << xx+mx << endl;
+
+#define fr(i, a, b, c) for(int i=a; i<b; i+=c)
+#define rfr(i, a, b, c) for(int i=a; i>=b;i-=c)
+
+#define scn(x) cin >> x
+#define scna(a) for(int i=0; i<n; i++){cin >> a[i];}
+#define pr(x) cout << x << endl
+#define pra(a) for(auto x:a){cout << x << " ";}nl
+
+ll mod = 1000000007;
+
+/*
+
+*/
+
 #define N 200005
-#define LOG 19
 
-vector<int> G[N];
-int dp[N], L[N];
-int LCA[N][LOG+1];
-
-int dfs(int x, int lvl, int p){
-    LCA[x][0] = p;
-    L[x] = lvl;
-    dp[x] = 1;
-    for(int t:G[x]){
-        if(t!=p){
-            dp[x]+=dfs(t, lvl+1, x);
-        }
-    }
-    return dp[x];
+vi ar[N];
+const int maxN = 19;
+int level[N] , LCA[N][maxN+1], dp[N], P[N];
+ 
+ 
+int dfs(int node , int lvl , int par){
+	level[node] = lvl;
+	LCA[node][0] = par;
+    dp[node] = 1;
+	for(int child : ar[node]){
+    	if(child != par){
+    	    P[child] = node;
+    		dp[node] += dfs(child , lvl+1 , node);
+    	}
+	}
+	return dp[node];
 }
-
+ 
+ 
 void init(int n){
-    for(int i=0; i<=LOG; i++){
-        for(int j=1; j<=n; j++){
-            LCA[j][i] = -1;
+    for(int i=1;i<=n;i++){
+		for(int j=0;j<=maxN;j++){
+		    LCA[i][j] = -1;
         }
-    }
-    dfs(1,0,-1);
-    for(int i=1; i<=LOG; i++){
-        for(int j=1; j<=n; j++){
-            if(LCA[j][i-1]!=-1){
-                LCA[j][i] = LCA[LCA[j][i-1]][i-1];
-            }
-        }
-    }
+	}
+	dfs(1 , 0 , -1);
+ 
+	for(int i=1;i<=maxN;i++)
+	{
+		for(int j=1;j<=n;j++)
+		if(LCA[j][i-1] != -1)
+		{
+			int par = LCA[j][i-1];
+			LCA[j][i] = LCA[par][i-1];
+		}
+	}
+}
+ 
+int getLCA(int a , int b)
+{
+	if(level[b] < level[a]) swap(a , b);
+ 
+	int d = level[b] - level[a];
+ 
+	while(d > 0)
+	{
+		int i = log2(d);
+		b = LCA[b][i];
+ 
+		d -= 1 << i;
+	}
+ 
+	if(a == b) return a;
+ 
+	for(int i=maxN;i>=0;i--)
+	if(LCA[a][i] != -1 && (LCA[a][i] != LCA[b][i]))
+	{
+		a = LCA[a][i] , b = LCA[b][i];
+	}
+ 
+	return LCA[a][0];
+}
+ 
+ 
+int getDist(int a , int b)
+{
+	int lca = getLCA(a , b);
+	int dist = level[a] + level[b] - 2*level[lca];
+	return dist;
 }
 
-int getLCA(int u, int v){
-    if(L[v]<L[u]){
-        swap(u,v);
-    }
-    int d = L[v]-L[u];
-    for(int i=LOG; i>=0; i--){
-        if(d&(1<<i)){
-            v = LCA[v][i];
-        }
-    }
-    if(u==v){
-        return v;
-    }
-    for(int i=LOG; i>=0; i--){
-        if(LCA[u][i]!=-1 && LCA[v][i]!=LCA[u][i]){
-            u = LCA[u][i];
-            v = LCA[v][i];
-        }
-    }
-    return LCA[u][0];
-}
-
-int getDist(int u, int v){
-    int lca = getLCA(u,v);
-    return L[u]+L[v]-2*L[lca];
-}
-int main(){
-    int n; cin >> n;
-    for(int i=1; i<n; i++){
+void solve(int xx){ 
+    int n,q; cin >> n;
+    fr(i,0,n-1,1){
         int u,v; cin >> u >> v;
-        G[u].push_back(v);
-        G[v].push_back(u);
+        ar[u].pb(v);
+        ar[v].pb(u);
     }
+    cin >> q;
     init(n+1);
-    int q; cin >> q;
     while(q--){
         int u,v; cin >> u >> v;
-        int d = getDist(u,v);
-        if(d%2){
-            cout << 0 << endl;
+        int dd = getDist(u,v);
+        if(dd%2){
+            pr(0);
         }
         else if(u==v){
-            cout << n << endl;
+            pr(n);
         }
-        else if(L[u]==L[v]){
-            int g = (d/2)-1;
-            for(int i=LOG; i>=0; i--){
-                if(g&(1<<i)){
-                    u = LCA[u][i];
-                    v = LCA[v][i];
-                }
-            }
-            cout << dp[1]-dp[u]-dp[v] << endl;
+        else if(level[u]==level[v]){
+            int g = getDist(u,v); 
+            g/=2; g--;
+            int aa = u, bb = v;
+            while(g>0){
+        		int i = log2(g);
+        		aa = LCA[aa][i];
+        		bb = LCA[bb][i];
+        		g -= 1 << i;
+        	}
+        	int lca = getLCA(u,v);
+        	int ans = dp[lca]-dp[aa]-dp[bb];
+            pr(dp[1]-dp[lca]+ans);
         }
         else{
-            if(L[v]<L[u]){
-                swap(u,v);
-            }
-            int jump = d/2-1;
-            for(int i=LOG; i>=0; i--){
-                if(jump&(1<<i)){
-                    v = LCA[v][i];
-                }
-            }
-            int par_v = LCA[v][0];
-            cout << dp[par_v]-dp[v] << endl;
+            if(level[v] < level[u]) swap(u,v);
+        	int d = dd/2; int tmp = v;
+        	while(d>0){
+        		int i = log2(d);
+        		v = LCA[v][i];
+        		d -= 1 << i;
+        	}
+        	d = dd/2-1;
+        	while(d>0){
+        		int i = log2(d);
+        		tmp = LCA[tmp][i];
+        		d -= 1 << i;
+        	}
+        	int ans = dp[v];
+        	ans-=dp[tmp];
+        	pr(ans);
         }
     }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t, i=0; 
+    // cin >> t;
+    // while(t--)  
+        solve(++i);
+    return 0;
 }
